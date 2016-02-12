d7036920574736ef3b4aadc5cb768cf0d69ded93
Caleb Anderson <robotrising@gmail.com>
2016-02-11 23:35:41 -0700

    camera stuff. rudimentary player controller

---
+++ Assets/PlayerInput.cs
@@ -0,0 +1,47 @@
+﻿using UnityEngine;
+
+namespace Assets
+{
+    public class PlayerInput : MonoBehaviour
+    {
+        // player mass is 75
+        public float MoveForce = 75;
+        public float BodyDrag = 0.05f;
+        public float LimbDrag = 0.5f;
+
+        public Rigidbody Driver { get; private set; }
+
+        void Start()
+        {
+            var player = transform.root.gameObject;
+            var rbs = player.GetComponentsInChildren<Rigidbody>();
+            foreach (var rb in rbs)
+            {
+                rb.useGravity = false;
+                rb.drag = LimbDrag;
+                rb.angularDrag = 0.05f;
+                rb.isKinematic = false;
+
+                if (rb.name == "EthanSpine")
+                {
+                    Driver = rb;
+                    rb.drag = BodyDrag;
+                }
+            }
+        }
+    
+        // We're applying physics updates, so use FixedUpdate
+        void FixedUpdate()
+        {
+            var x = Input.GetAxis("Horizontal");
+            var y = Input.GetAxis("Normal");
+            var z = Input.GetAxis("Vertical");
+
+            var right = Camera.main.transform.right * x;
+            var up = Camera.main.transform.up * y;
+            var forward = Camera.main.transform.forward * z;
+            var dir = (right + up + forward) * MoveForce;
+            Driver.AddForce(dir);
+        }
+    }
+}
---
+++ Assets/Scripts/CameraController.cs
@@ -0,0 +1,45 @@
+﻿using UnityEngine;
+
+namespace Assets.Scripts
+{
+    public class CameraController : MonoBehaviour
+    {
+        public GameObject Target = null;
+        public Rigidbody TargetRigidbody;
+        public float OffsetDistance = 6;
+        public float MinOffset = 1;
+        public float MaxOffset = 10;
+
+        private float _azimuth = 0;
+        private float _elevation = 0;
+        private float _dampVelocity;
+        private float _targetOffset;
+
+        void Start()
+        {
+            _targetOffset = OffsetDistance;
+            var pm = Target.GetComponentInChildren<PlayerInput>();
+            TargetRigidbody = pm.Driver;
+        }
+
+        // Update is called once per frame
+        void Update()
+        {
+            var az = Input.GetAxis("Mouse X");
+            var el = Input.GetAxis("Mouse Y");
+            var zm = Input.GetAxis("Mouse ScrollWheel") * 10f;
+            
+            _targetOffset = Mathf.Clamp(_targetOffset - zm, MinOffset, MaxOffset);
+
+            OffsetDistance = Mathf.SmoothDamp(OffsetDistance, _targetOffset, ref _dampVelocity, 0.3f);
+
+            _azimuth += az;
+            _elevation += el;
+            
+            var rot = Quaternion.Euler(_elevation, _azimuth, 0);
+            var pos = rot * new Vector3(0, 0, -OffsetDistance) + TargetRigidbody.position;
+            transform.rotation = rot;
+            transform.position = pos;
+        }
+    }
+}
