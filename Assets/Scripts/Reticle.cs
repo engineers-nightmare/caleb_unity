873fcfb01ecd7237d82f48d71e5970207a9a7944
Chris Forbes <chrisf@ijw.co.nz>
2016-03-19 18:46:02 +1300

    Add reticle for aiming
    It's really hard to guess where the center point is otherwise.

---
+++ Assets/Scripts/Reticle.cs
@@ -0,0 +1,13 @@
+﻿using UnityEngine;
+using System.Collections;
+
+public class Reticle : MonoBehaviour
+{
+
+    public Texture2D Tex;
+
+    void OnGUI()
+    {
+        GUI.DrawTexture(new Rect((Screen.width - Tex.width) / 2, (Screen.height - Tex.height) / 2, Tex.width, Tex.height), Tex);
+    }
+}
