cb20406f7c506065d49b6424296b1636039f387d
Chris Forbes <chrisf@ijw.co.nz>
2016-03-19 14:00:38 +1300

    Initial import into git
    Signed-off-by: Chris Forbes <chrisf@ijw.co.nz>

---
+++ Assets/Scripts/ChunkData.cs
@@ -0,0 +1,23 @@
+﻿using UnityEngine;
+
+public class ChunkData : MonoBehaviour
+{
+
+    public byte[,,] Contents = new byte[Constants.ChunkSize, Constants.ChunkSize, Constants.ChunkSize];
+    public int generation;
+
+    ChunkData()
+    {
+        // some initial data
+        Contents[0, 0, 0] = 1;
+        Contents[0, 0, 1] = 1;
+        Contents[0, 1, 1] = 1;
+        generation = 0;
+    }
+
+    // Use this for initialization
+    void Start()
+    {
+        
+    }
+}
---
+++ Assets/Scripts/ChunkMesher.cs
@@ -0,0 +1,72 @@
+﻿using UnityEngine;
+using System.Collections.Generic;
+
+public class ChunkMesher : MonoBehaviour
+{
+    public ChunkData Data;
+    int? generation;
+
+    public Mesh FrameMeshTemplate;
+    public Mesh[] FaceMeshTemplates = new Mesh[6];
+    public MeshFilter OutputMeshFilter;
+
+    // Use this for initialization
+    void Start()
+    {
+        generation = null;
+    }
+
+    void RebuildTargetMesh()
+    {
+        var verts = new List<Vector3>();
+        var uvs = new List<Vector2>();
+        var normals = new List<Vector3>();
+        var indices = new List<int>();
+
+        var templateMeshVerts = FrameMeshTemplate.vertices;
+        var templateMeshIndices = FrameMeshTemplate.triangles;
+        var templateMeshUvs = FrameMeshTemplate.uv;
+        var templateMeshNormals = FrameMeshTemplate.normals;
+
+        for (int i = 0; i < Constants.ChunkSize; i++)
+            for (int j = 0; j < Constants.ChunkSize; j++)
+                for (int k = 0; k < Constants.ChunkSize; k++)
+                    if (Data.Contents[i, j, k] != 0)
+                    {
+                        var p = new Vector3(i + 1, j, k);       // HACK for broken content.
+                        var indexOffset = verts.Count;
+
+                        foreach (var v in templateMeshVerts)
+                            verts.Add(p + v);
+                        foreach (var uv in templateMeshUvs)
+                            uvs.Add(uv);
+                        foreach (var n in templateMeshNormals)
+                            normals.Add(n);
+                        foreach (var ind in templateMeshIndices)
+                            indices.Add(indexOffset + ind);
+                    }
+
+        var m = new Mesh();
+        m.SetVertices(verts);
+        m.SetUVs(0, uvs);
+        m.SetNormals(normals);
+        m.SetTriangles(indices, 0);
+        OutputMeshFilter.mesh = m;
+    }
+
+    void OnDrawGizmos()
+    {
+        Gizmos.matrix = transform.localToWorldMatrix;
+        Gizmos.DrawWireCube(new Vector3(Constants.ChunkSize / 2, Constants.ChunkSize / 2, Constants.ChunkSize / 2),
+            new Vector3(Constants.ChunkSize, Constants.ChunkSize, Constants.ChunkSize));
+    }
+
+    void Update()
+    {
+        if (generation != Data.generation)
+        {
+            RebuildTargetMesh();
+            generation = Data.generation;
+        }
+    }
+}
