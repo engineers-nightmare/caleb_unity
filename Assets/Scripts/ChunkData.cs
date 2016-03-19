8ae8feb7959b5249ac56604ac2683aa75f131ac5
Chris Forbes <chrisf@ijw.co.nz>
2016-03-19 15:03:10 +1300

    Add chunk face crossing raycast.

--- Assets/Scripts/ChunkData.cs
+++ Assets/Scripts/ChunkData.cs
@@ -1,8 +1,22 @@
 ﻿using UnityEngine;
+using System.Collections.Generic;
+
+public struct BlockHit
+{
+    public IntVec3 pos;
+    public IntVec3 normal;
+    public float t;
+
+    public BlockHit(IntVec3 pos, IntVec3 normal, float t)
+    {
+        this.pos = pos;
+        this.normal = normal;
+        this.t = t;
+    }
+}
 
 public class ChunkData : MonoBehaviour
 {
-
     public byte[,,] Contents = new byte[Constants.ChunkSize, Constants.ChunkSize, Constants.ChunkSize];
     public int generation;
 
@@ -15,9 +29,66 @@ public class ChunkData : MonoBehaviour
         generation = 0;
     }
 
-    // Use this for initialization
-    void Start()
+    static float MaxAlongAxis(float a, float d)
     {
-        
+        if (d > 0)
+        {
+            return Mathf.Abs((Mathf.Ceil(a) - a) / d);
+        }
+        else
+        {
+            return Mathf.Abs((Mathf.Floor(a) - a) / d);
+        }
+    }
+
+    public IEnumerable<BlockHit> BlockCrossingsLocalSpace(Vector3 start, Vector3 dir, float maxDistance)
+    {
+        // Produces a sequence of block face crossings. This is more or less DDA with some extra tracking.
+        var t = 0.0f;
+        var bl = (start + new Vector3(Constants.ChunkSize / 2, Constants.ChunkSize / 2, Constants.ChunkSize / 2)).FloorToInt();
+        var n = new IntVec3();
+        var step = dir.SignToInt();
+        // Distance in t between successive crossings on each axis
+        var delta = new Vector3(
+            Mathf.Abs(1.0f / dir.x),
+            Mathf.Abs(1.0f / dir.y),
+            Mathf.Abs(1.0f / dir.z));
+        // First crossings in t on each axis
+        var max = new Vector3(
+            MaxAlongAxis(start.x, dir.x),
+            MaxAlongAxis(start.y, dir.y),
+            MaxAlongAxis(start.z, dir.z));
+
+        // Repeatedly cross the next closest face.
+        while (t < maxDistance)
+        {
+            if (max.x <= max.y && max.x <= max.z)
+            {
+                bl.x += step.x;
+                n = new IntVec3(-step.x, 0, 0);
+                t = max.x;
+                max.x += delta.x;
+            }
+            else if (max.y < max.z)
+            {
+                bl.y += step.y;
+                n = new IntVec3(-step.y, 0, 0);
+                t = max.y;
+                max.y += delta.y;
+            }
+            else
+            {
+                bl.z += step.z;
+                n = new IntVec3(-step.z, 0, 0);
+                t = max.z;
+                max.z += delta.z;
+            }
+
+            if (bl.x < 0 || bl.x >= Constants.ChunkSize) continue;
+            if (bl.y < 0 || bl.y >= Constants.ChunkSize) continue;
+            if (bl.z < 0 || bl.z >= Constants.ChunkSize) continue;
+
+            yield return new BlockHit(bl, n, t);
+        }
     }
 }
---
+++ Assets/Scripts/Math.cs
@@ -0,0 +1,52 @@
+﻿using UnityEngine;
+
+public struct IntVec3
+{
+    public int x, y, z;
+
+    public IntVec3(int x, int y, int z)
+    {
+        this.x = x;
+        this.y = y;
+        this.z = z;
+    }
+
+    public static bool operator ==(IntVec3 a, IntVec3 b)
+    {
+        return a.x == b.x && a.y == b.y && a.z == b.z;
+    }
+
+    public static bool operator !=(IntVec3 a, IntVec3 b)
+    {
+        return a.x != b.x || a.y != b.y || a.z != b.z;
+    }
+
+    public override bool Equals(object obj)
+    {
+        return this == (IntVec3)obj;
+    }
+
+    public override int GetHashCode()
+    {
+        return x.GetHashCode() ^ y.GetHashCode() ^ z.GetHashCode();
+    }
+}
+
+public static class MathExts
+{
+    public static IntVec3 FloorToInt(this Vector3 p)
+    {
+        return new IntVec3(
+            Mathf.FloorToInt(p.x),
+            Mathf.FloorToInt(p.y),
+            Mathf.FloorToInt(p.z));
+    }
+
+    public static IntVec3 SignToInt(this Vector3 p)
+    {
+        return new IntVec3(
+            (int)Mathf.Sign(p.x),
+            (int)Mathf.Sign(p.y),
+            (int)Mathf.Sign(p.z));
+    }
+}
