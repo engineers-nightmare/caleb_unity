4c3bb231d52d4bffddd76bcf80ce48ec8def587b
Caleb Anderson <robotrising@gmail.com>
2016-03-28 00:27:18 -0600

    build tool: add ability to swap modes
    swaps between frame and surface
    primary and secondary tool use button (m1, m2 by default) will
    place/remove selected mode

--- Assets/Scripts/Tools/BuildTool.cs
+++ Assets/Scripts/Tools/BuildTool.cs
@@ -13,6 +13,14 @@ public class BuildTool : MonoBehaviour
     public Mesh FrameMesh = null;
     public Material PreviewMaterial = null;
 
+    public BuildToolMode ToolMode = BuildToolMode.Frame;
+
+    public enum BuildToolMode
+    {
+        Frame,
+        Surface,
+    }
+
     static int NormalToFaceIndex(IntVec3 n)
     {
         if (n.x == -1) return 0;
@@ -28,15 +36,9 @@ public class BuildTool : MonoBehaviour
     void Start()
     {
     }
-
-    protected void UpdatePreview()
+    private void UpdatePreview(Ray ray, Vector3 rayOriginLocalSpace, Vector3 rayDirLocalSpace)
     {
-        var ray = Camera.main.ScreenPointToRay(
-            new Vector3(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2, 0));
-
         var ceTrans = ChunkMapToEdit.transform;
-        var rayOriginLocalSpace = ceTrans.InverseTransformPoint(ray.origin);
-        var rayDirLocalSpace = ceTrans.InverseTransformDirection(ray.direction);
 
         foreach (var fc in ChunkData.BlockCrossingsLocalSpace(rayOriginLocalSpace, rayDirLocalSpace, 5.0f))
         {
@@ -55,84 +57,156 @@ public class BuildTool : MonoBehaviour
         }
     }
 
+    void OnGUI()
+    {
+        GUI.Label(new Rect(10, 10, 100, 20), ToolMode.ToString());
+    }
+
     void OnDrawGizmos()
     {
         // had to gut this.
     }
 
-    void Update()
+    private void Update()
     {
+        if (Input.GetButtonDown("ToolModeSwitch"))
+        {
+            if (ToolMode == BuildToolMode.Frame)
+            {
+                ToolMode = BuildToolMode.Surface;
+            }
+            else if (ToolMode == BuildToolMode.Surface)
+            {
+                ToolMode = BuildToolMode.Frame;
+            }
+        }
+
         var ray = Camera.main.ScreenPointToRay(new Vector3(Camera.main.pixelWidth / 2, Camera.main.pixelHeight / 2, 0));
 
         var rayOriginLocalSpace = ChunkMapToEdit.transform.InverseTransformPoint(ray.origin);
         var rayDirLocalSpace = ChunkMapToEdit.transform.InverseTransformDirection(ray.direction);
 
-        UpdatePreview();
 
-        // Surface placement
-        if (Input.GetButtonDown("ToolTertiary"))
-        {
-            foreach (var fc in ChunkData.BlockCrossingsLocalSpace(rayOriginLocalSpace, rayDirLocalSpace, 5.0f))
-            {
-                var ce = ChunkMapToEdit.GetChunk(IntVec3.BlockCoordToChunkCoord(fc.pos));
-                var co = IntVec3.BlockCoordToChunkOffset(fc.pos);
-                if (ce != null && ce.Contents[co.x, co.y, co.z] != 0)
-                {
-                    var faceIndex = NormalToFaceIndex(fc.normal);
-                    if (ce.Faces[co.x, co.y, co.z, faceIndex] == 0)
-                    {
-                        ce.Faces[co.x, co.y, co.z, faceIndex] = 1;
-                        ce.generation++;
-
-                        AudioSource.PlayClipAtPoint(RemoveBlockSound, ray.origin + fc.t * ray.direction);
-                        break;
-                    }
-                }
-            }
-        }
-
-        // Remove block tool -- removes the first block we hit
         if (Input.GetButtonDown("ToolPrimary"))
         {
-            foreach (var fc in ChunkData.BlockCrossingsLocalSpace(rayOriginLocalSpace, rayDirLocalSpace, 5.0f))
+            switch (ToolMode)
             {
-                var ce = ChunkMapToEdit.GetChunk(IntVec3.BlockCoordToChunkCoord(fc.pos));
-                var co = IntVec3.BlockCoordToChunkOffset(fc.pos);
-                if (ce != null && ce.Contents[co.x, co.y, co.z] != 0)
+                case BuildToolMode.Frame:
+                    PlaceFrame(ray, rayOriginLocalSpace, rayDirLocalSpace);
+                    break;
+                case BuildToolMode.Surface:
+                    PlaceSurface(ray, rayOriginLocalSpace, rayDirLocalSpace);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        else if (Input.GetButtonDown("ToolSecondary"))
+        {
+            switch (ToolMode)
+            {
+                case BuildToolMode.Frame:
+                    RemoveFrame(ray, rayOriginLocalSpace, rayDirLocalSpace);
+                    break;
+                case BuildToolMode.Surface:
+                    RemoveSurface(ray, rayOriginLocalSpace, rayDirLocalSpace);
+                    break;
+                default:
+                    throw new ArgumentOutOfRangeException();
+            }
+        }
+        else if (Input.GetButtonDown("ToolTertiary"))
+        {
+        }
+
+        UpdatePreview(ray, rayOriginLocalSpace, rayDirLocalSpace);
+    }
+
+    // Surface placement
+    void PlaceSurface(Ray ray, Vector3 rayOriginLocalSpace, Vector3 rayDirLocalSpace)
+    {
+        foreach (var fc in ChunkData.BlockCrossingsLocalSpace(rayOriginLocalSpace, rayDirLocalSpace, 5.0f))
+        {
+            var ce = ChunkMapToEdit.GetChunk(IntVec3.BlockCoordToChunkCoord(fc.pos));
+            var co = IntVec3.BlockCoordToChunkOffset(fc.pos);
+            if (ce != null && ce.Contents[co.x, co.y, co.z] != 0)
+            {
+                var faceIndex = NormalToFaceIndex(fc.normal);
+                if (ce.Faces[co.x, co.y, co.z, faceIndex] == 0)
                 {
-                    ce.Contents[co.x, co.y, co.z] = 0;
+                    ce.Faces[co.x, co.y, co.z, faceIndex] = 1;
                     ce.generation++;
 
-                    // Emit noise
                     AudioSource.PlayClipAtPoint(RemoveBlockSound, ray.origin + fc.t * ray.direction);
-
                     break;
                 }
             }
         }
+    }
 
-        // Place block tool -- places a block against the block we hit, sharing the face we hit
-        if (Input.GetButtonDown("ToolSecondary"))
+    // Surface removal
+    void RemoveSurface(Ray ray, Vector3 rayOriginLocalSpace, Vector3 rayDirLocalSpace)
+    {
+        foreach (var fc in ChunkData.BlockCrossingsLocalSpace(rayOriginLocalSpace, rayDirLocalSpace, 5.0f))
         {
-            foreach (var fc in ChunkData.BlockCrossingsLocalSpace(rayOriginLocalSpace, rayDirLocalSpace, 5.0f))
+            var ce = ChunkMapToEdit.GetChunk(IntVec3.BlockCoordToChunkCoord(fc.pos));
+            var co = IntVec3.BlockCoordToChunkOffset(fc.pos);
+            if (ce != null && ce.Contents[co.x, co.y, co.z] != 0)
             {
-                var ce = ChunkMapToEdit.GetChunk(IntVec3.BlockCoordToChunkCoord(fc.pos));
-                var co = IntVec3.BlockCoordToChunkOffset(fc.pos);
-                if (ce != null && ce.Contents[co.x, co.y, co.z] != 0)
+                var faceIndex = NormalToFaceIndex(fc.normal);
+                if (ce.Faces[co.x, co.y, co.z, faceIndex] != 0)
                 {
-                    // Step back along normal.
-                    var p = fc.pos + fc.normal;
-                    var ce2 = ChunkMapToEdit.EnsureChunk(IntVec3.BlockCoordToChunkCoord(p));
-                    var co2 = IntVec3.BlockCoordToChunkOffset(p);
+                    ce.Faces[co.x, co.y, co.z, faceIndex] = 0;
+                    ce.generation++;
 
-                    ce2.Contents[co2.x, co2.y, co2.z] = 1;
-                    ce2.generation++;
-
-                    // Emit clunk noise
-                    AudioSource.PlayClipAtPoint(PlaceBlockSound, ray.origin + fc.t * ray.direction);
-
+                    AudioSource.PlayClipAtPoint(RemoveBlockSound, ray.origin + fc.t * ray.direction);
                     break;
                 }
+            }
+        }
+    }
+
+    // Place block tool -- places a block against the block we hit, sharing the face we hit
+    void PlaceFrame(Ray ray, Vector3 rayOriginLocalSpace, Vector3 rayDirLocalSpace)
+    {
+        foreach (var fc in ChunkData.BlockCrossingsLocalSpace(rayOriginLocalSpace, rayDirLocalSpace, 5.0f))
+        {
+            var ce = ChunkMapToEdit.GetChunk(IntVec3.BlockCoordToChunkCoord(fc.pos));
+            var co = IntVec3.BlockCoordToChunkOffset(fc.pos);
+            if (ce != null && ce.Contents[co.x, co.y, co.z] != 0)
+            {
+                // Step back along normal.
+                var p = fc.pos + fc.normal;
+                var ce2 = ChunkMapToEdit.EnsureChunk(IntVec3.BlockCoordToChunkCoord(p));
+                var co2 = IntVec3.BlockCoordToChunkOffset(p);
+
+                ce2.Contents[co2.x, co2.y, co2.z] = 1;
+                ce2.generation++;
+
+                // Emit clunk noise
+                AudioSource.PlayClipAtPoint(PlaceBlockSound, ray.origin + fc.t * ray.direction);
+
+                break;
+            }
+        }
+    }
+
+    // Remove block tool -- removes the first block we hit
+    void RemoveFrame(Ray ray, Vector3 rayOriginLocalSpace, Vector3 rayDirLocalSpace)
+    {
+        foreach (var fc in ChunkData.BlockCrossingsLocalSpace(rayOriginLocalSpace, rayDirLocalSpace, 5.0f))
+        {
+            var ce = ChunkMapToEdit.GetChunk(IntVec3.BlockCoordToChunkCoord(fc.pos));
+            var co = IntVec3.BlockCoordToChunkOffset(fc.pos);
+            if (ce != null && ce.Contents[co.x, co.y, co.z] != 0)
+            {
+                ce.Contents[co.x, co.y, co.z] = 0;
+                ce.generation++;
+
+                // Emit noise
+                AudioSource.PlayClipAtPoint(RemoveBlockSound, ray.origin + fc.t * ray.direction);
+
+                break;
             }
         }
     }
